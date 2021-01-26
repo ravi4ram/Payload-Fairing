1c9fcea3d3158045659b0dc36badfb0791401c01
ravi4ram <ravindran_ram@yahoo.com>
2021-01-26 21:12:03 +0530

    Modified for Current Composite Fairing

--- payload_fairing.py
+++ payload_fairing.py
@@ -3,13 +3,24 @@ import numpy as np
 
 # payload fairing (spherically blunted tangent ogive)
 def calc_payload_fairing(diameter):	
-	R = 0.5 * diameter				# radius
-	Lo= 1.225 * diameter			# Length of ogive part
-	rho= ( R**2 + Lo**2 ) / (2*R)	# rho = (R^2 + L^2)/2R
-	rn = 0.28 * diameter			# nose radius
 
 	# output values
 	x_out = []; y_out = []; yne_out = []
+
+	"""
+	sphere radius, rs = 1.330
+	tangency point, xt = 0.6615
+	ogive length, Lo = 1.225 * D
+	frustum length,  Lf = 0.2576 * D = 1.288
+	aft frustum dia, xf = 0.8208 * D = 4.104
+	cylinder length, Lc = 0.75 * D = 3.750	
+	"""
+	R = 0.5 * diameter		# fairing radius
+	rho= 10					# ogive radius
+	rn = 0.266 * diameter	# nose radius
+
+	# rho = ( R**2 + Lo**2 ) / (2*R)
+	Lo = np.sqrt( (rho * diameter) - R**2)
 
 	# tangency point (circle meets ogive)
 	xo = Lo - np.sqrt((rho - rn)**2 - (rho - R)**2)
@@ -45,11 +56,11 @@ def calc_payload_fairing(diameter):
 	x_out = np.append(x_out, x)
 	y_out = np.append(y_out, y)
 	yne_out=np.append(yne_out, yne)		
-	
+
 	# frustum part
 	x_i = x_out[-1]; y_i = y_out[-1];
-	Lf = Lc + (0.275 * diameter)	# Length of frustum part
-	x_end = Lf; y_end = 0.8 * diameter/2;
+	Lf = Lc + (0.2576 * diameter)	# Length of frustum part
+	x_end = Lf; y_end = 0.8208 * diameter/2;
 	# equation of the slanting line
 	slope = (y_end - y_i) / (x_end - x_i)
 	intercept = ( (x_end * y_i) - (x_i * y_end) ) / ( x_end - x_i)
@@ -61,18 +72,25 @@ def calc_payload_fairing(diameter):
 	x_out = np.append(x_out, x)
 	y_out = np.append(y_out, y)
 	yne_out=np.append(yne_out, yne)	
-	
+
 	# other dimensions
 	a_dim = (R, Lo, rn, rho, xo, xa, xt, yt)
 	
 	return a_dim, x_out, y_out, yne_out
 
-	
 # find the nearest index in the list for the given value
 def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx, array[idx]	
+
+# ar_from = [0, -0.2]; ar_to = [xo, -0.2]; text_loc=[xo/2-0.5, -0.5]
+# text = ' xo = '+ str(round( xo,1 ) )
+def arrow(ax, ar_from, ar_to, text, text_loc): 
+	ax.plot(ar_from[0], ar_from[1], '.' ); ax.plot(ar_to[0], ar_to[1], '+' );
+	ax.annotate( "", ar_from, ar_to , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
+	ax.text(text_loc[0], text_loc[1], text, fontsize=9 )	
+	return
 
 # 2D plot
 def plot2D(ax, d, x, y, yne, ifdim=False):
@@ -88,15 +106,11 @@ def plot2D(ax, d, x, y, yne, ifdim=False):
 	if ifdim:
 		# apex length
 		text = ' xa = '+ str(round( xa,1 ) )
-		ax.plot(0, 0, '.' ); ax.plot(xa, 0, '+' );
-		ax.annotate( "", [0, 0], [xa, 0] , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
-		ax.text(0.1, -0.1, text, fontsize=9 )
+		arrow(ax, [0, 0], [xa, 0], text, [0.1, -0.1])
 				
 		# center of the spherical nose cap
 		text = ' xo = '+ str(round( xo,1 ) ) 
-		ax.plot(0, -0.2, '.' ); ax.plot(xo, -0.2, '+' );
-		ax.annotate( "", [0, -0.2], [xo, -0.2] , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
-		ax.text(xo/2-0.5, -0.5, text, fontsize=9 )
+		arrow(ax, [0, -0.2], [xo, -0.2], text, [xo/2-0.5, -0.5])
 		
 		# tangency point
 		text = ' [xt, yt] = {'+ str(round(xt,1)) +','+ str(round(-yt,1)) + '} '
@@ -104,55 +118,38 @@ def plot2D(ax, d, x, y, yne, ifdim=False):
 		ax.text(-xt+2, -yt-0.5, text, fontsize=9 )
 				
 		# blunt sphere radius, inlet radial line [xo, 0] to [xt, yt]
-		text = ' Rn = '+ str(round(rn,1)) + ' \n [0.28 * D] '
-		ax.plot(xo, 0, '.' ); ax.plot(xt, yt, '+' )
-		# draw dimension from [xo, 0] to [xt, yt]
-		ax.annotate( "", [xo, 0], [xt, yt] , arrowprops=dict(lw=0.5, arrowstyle='<-') )
-		ax.text((xo+xt)/2, (0+yt)/2, text, fontsize=9 )
+		text = ' Rn = '+ str(round(rn,1)) + ' \n [0.266 * D] '
+		arrow(ax, [xo, 0], [xt, yt], text, [(xo+xt)/2, (0+yt)/2])
 				
 		# ogive radius 
-		text = ' rho = '+ str(round(rho,1)) + ' \n [($R^2$ + $L^2$)/2R] ' #$10^1$
+		text = ' rho = '+ str(round(rho,1)) 
 		# array mid point value
-		x_near = Lo/2
-		x_index, x_middle = find_nearest(x, x_near)		
+		x_index, x_middle = find_nearest(x, Lo/2)		
 		y_middle = y[x_index]
-		ax.plot(Lo, rho-R, '+' ); ax.plot(x_middle, -y_middle, '+' )
-		ax.annotate( "", [Lo, rho-R], [x_middle, -y_middle] , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
-		ax.text((Lo+x_middle)/2-0.8, ((rho-R)-y_middle)/2+0.5, text, fontsize=9 )
+		arrow(ax, [Lo, rho-R], [x_middle, -y_middle], text, [(Lo+x_middle)/2-0.8, ((rho-R)-y_middle)/2+0.5])
 		
 		# diameter
-		text = ' R = '+ str(round(R,1)) + ' '
-		ax.plot(Lo, 0, '.' ); ax.plot(Lo, R, '+' );
-		ax.annotate( "", [Lo, 0], [Lo, R] , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
-		ax.text(Lo, R/2, text, fontsize=9 )
+		text = ' R = '+ str(round(R,1))
+		arrow(ax, [Lo, 0], [Lo, R], text, [Lo, R/2])
 				
 		# ogive length
-		text = ' Lo = '+ str(round( Lo,1 ) ) + ' \n [1.225 * D] '
-		ax.plot(0, -0.4, '.' ); ax.plot(Lo, -0.4, '+' );
-		ax.annotate( "", [0, -0.4], [Lo, -0.4] , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
-		ax.text((Lo)/2, -0.7, text, fontsize=9 )
+		text = ' Lo = '+ str(round( Lo,1 ) ) + ' \n sqrt( (rho * diameter) - $R^2$) '
+		arrow(ax, [0, -0.4], [Lo, -0.4] , text, [(Lo)/2, -0.7])
 		
 		# cylinder length
 		Lc = Lo + (0.75 * 2 * R)	
 		text = ' Lc = '+ str(round( (0.75 * 2 * R),1 ) ) + ' \n [0.75 * D] '
-		ax.plot(Lo, -0.3, '.' ); ax.plot(Lc, -0.3, '+' );
-		ax.annotate( "", [Lo, -0.3], [Lc, -0.3] , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
-		ax.text((Lo+Lc)/2, -0.6, text, fontsize=9 )
+		arrow(ax, [Lo, -0.3], [Lc, -0.3] , text, [(Lo+Lc)/2, -0.6])
 		
 		# frustum length
-		Lf = Lc + (0.275 *  2 * R)
-		text = ' Lf = '+ str(round( (0.275 * 2 * R),1 ) ) + ' \n [0.275 * D] '
-		ax.plot(Lc, -0.2, '.' ); ax.plot(Lf, -0.2, '+' );
-		ax.annotate( "", [Lc, -0.2], [Lf, -0.2] , arrowprops=dict(lw=0.5, arrowstyle='<-') )
-		ax.text((Lc+Lf)/2, -0.5, text, fontsize=9 )
+		Lf = Lc + (0.2576 *  2 * R)
+		text = ' Lf = '+ str(round( (0.2576 * 2 * R),1 ) ) + ' \n [0.2576 * D] '
+		arrow(ax, [Lc, -0.2], [Lf, -0.2], text, [(Lc+Lf)/2, -0.5])
 		
 		# end diameter
-		Rt = 0.8 * R
-		text = ' Re = '+ str(round(Rt,1)) + ' '
-		Lt = 2.25 * 2 * R
-		ax.plot(Lt, 0, '.' ); ax.plot(Lt, Rt, '+' );
-		ax.annotate( "", [Lt, 0], [Lt, Rt] , arrowprops=dict(lw=0.5, arrowstyle='<-') )	
-		ax.text(Lt, Rt/2, text, fontsize=9 )		
+		Rt = 0.8208 * R
+		text = ' Re = '+ str(round(Rt,1))	
+		arrow(ax, [Lf, 0], [Lf, Rt], text, [Lf, Rt/2])	
 
 	# axis
 	ax.axhline(color='black', lw=0.5, linestyle="dashed")
@@ -201,7 +198,7 @@ def _set_axes_radius(ax, origin, radius):
     return
 
 # 3d plot
-def plot3D(ax, d, x, y, yne):
+def plot3D(ax, d, x, y, yne, ifdim=False):
 	# unpack the dimension values
 	R, Lo, rn, rho, xo, xa, xt, yt = d
 	
@@ -220,10 +217,7 @@ def plot3D(ax, d, x, y, yne):
 	ax.view_init(-170, -15)
 	return
 
-def plot(d, x, y, yne):
-	# set flag if dimensions in 2d plot are needed
-	ifdim = True
-		
+def plot(d, x, y, yne, ifdim=False):
 	# Plot 3d view
 	fig = plt.figure(figsize=(12,9))
 	# plot some 2d information
@@ -231,7 +225,7 @@ def plot(d, x, y, yne):
 	plot2D(ax1, d, x, y, yne, ifdim)
 	# plot 3d view
 	ax2 = fig.add_subplot(122, projection='3d')
-	plot3D(ax2, d, x, y, yne)	
+	plot3D(ax2, d, x, y, yne, ifdim)	
 	# show
 	fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 	plt.show()
@@ -243,8 +237,10 @@ if __name__ == '__main__':
 	
 	# calculate values
 	d, x, y, yne = calc_payload_fairing(fairing_dia)
-	
+
+	# set flag if dimensions in 2d plot are needed
+	ifdim = True	
 	# and plot it
-	plot(d, x, y, yne)
+	plot(d, x, y, yne, ifdim)
 
 
